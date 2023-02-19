ee0bda9e27cbc7f84e0345338c93ea61a11b45b6
gymneganesh <itznavaneeth@gmail.com>
2023-02-19 12:43:20 +1100

    NGT :  Initial commit - base project structure

---
+++ src/test/java/com/assessement/code/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessement.code.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
