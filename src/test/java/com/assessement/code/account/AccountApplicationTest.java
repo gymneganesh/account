16e879e123144c3cee37e6bdc210b8a63fd1b753
gymneganesh <itznavaneeth@gmail.com>
2023-02-26 14:37:19 +1100

    NGT :  Added unit tests for  AccountApplicationTest

---
+++ src/test/java/com/assessement/code/account/AccountApplicationTest.java
@@ -0,0 +1,25 @@
+package com.assessement.code.account;
+
+import com.assessement.code.account.api.AccountController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AccountApplicationTest {
+
+	@Autowired
+	AccountController accountController;
+
+	@Test
+	void contextLoads() {
+		assertNotNull(accountController);
+	}
+
+}
