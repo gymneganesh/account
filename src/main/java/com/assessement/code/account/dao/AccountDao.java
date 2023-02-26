b1ab82f20d225276178fef407d41cf00c22c54e5
gymneganesh <itznavaneeth@gmail.com>
2023-02-26 14:41:18 +1100

    NGT :  Updated Dao for Account

--- src/main/java/com/assessement/code/account/dao/AccountDao.java
+++ src/main/java/com/assessement/code/account/dao/AccountDao.java
@@ -1,12 +1,18 @@
 package com.assessement.code.account.dao;
 
+import com.assessement.code.account.dto.AccountDto;
 import com.assessement.code.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@Repository
 public interface AccountDao extends JpaRepository<Account, Long> {
-    List<Account> findByCustomerId(Long customerId);
+
+    @Query("SELECT new com.assessement.code.account.dto.AccountDto(a.accountNumber, a.accountName,a.accountType, a.currency, SUM(t.amount))" +
+            "from Account a " +
+            "LEFT JOIN Transaction t on t.accountId = a.accountNumber " +
+            "WHERE a.customerId = :customerId group by t.accountId")
+    List<AccountDto> findByCustomerId(@PathVariable("customerId") Long customerId);
 }
