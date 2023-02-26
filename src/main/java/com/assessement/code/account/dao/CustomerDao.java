f71ee4e9a5406f6b05a70429c5bf0797214104b6
gymneganesh <itznavaneeth@gmail.com>
2023-02-26 14:48:36 +1100

    NGT :  code formatting for CustomerDao

--- src/main/java/com/assessement/code/account/dao/CustomerDao.java
+++ src/main/java/com/assessement/code/account/dao/CustomerDao.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerDao extends JpaRepository<Customer, Long> {
-
     Optional<Customer> findById(Long id);
 }
