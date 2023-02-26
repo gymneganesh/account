2eccf416963f818c62e8a5243304a5e7e4730c6e
gymneganesh <itznavaneeth@gmail.com>
2023-02-26 14:34:28 +1100

    NGT :  Added entity class for Customer

--- src/main/java/com/assessement/code/account/entity/Customer.java
+++ src/main/java/com/assessement/code/account/entity/Customer.java
@@ -3,12 +3,14 @@ package com.assessement.code.account.entity;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 
     @Id
