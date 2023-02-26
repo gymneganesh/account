fd8fbec479b915d009a8d322bf772389731c6980
gymneganesh <itznavaneeth@gmail.com>
2023-02-26 14:49:42 +1100

    NGT :  updated the query for TransactionDao

--- src/main/java/com/assessement/code/account/dao/TransactionDao.java
+++ src/main/java/com/assessement/code/account/dao/TransactionDao.java
@@ -1,14 +1,21 @@
 package com.assessement.code.account.dao;
 
+import com.assessement.code.account.dto.AccountTransactionDto;
 import com.assessement.code.account.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@Repository
 public interface TransactionDao extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccountId(Long accountId);
+
+    @Query("SELECT new com.assessement.code.account.dto.AccountTransactionDto(t.accountId,t.date,t.amount,t.description,t.type, a.accountName, a.currency) " +
+            "from Transaction t " +
+            "JOIN Account a on t.accountId = a.accountNumber " +
+            "WHERE a.accountNumber=:accountId AND a.customerId = :customerId Order by t.date DESC")
+    List<AccountTransactionDto> findTransByAccountId(@PathVariable("customerId") Long customerId, @PathVariable("accountId") Long accountId);
+
 }
